9887125c63b963ab8e6b6b5f7dd41a88ff3869ae
chesheersky <d.f.goryunov@gmail.com>
2015-06-16 21:56:32 +0100

    Mocha plus two first steps

---
+++ DiamondExperiment/Modules/Diamond.js
@@ -0,0 +1,9 @@
+﻿var Diamond = function () { };
+
+Diamond.prototype.produce = function (char) {
+    if (char === 'B')
+        return " A \nB B\n A ";
+    return 'A';
+};
+
+module.exports = new Diamond();
---
+++ DiamondExperiment/Tests/MochaSuite.js
@@ -0,0 +1,18 @@
+﻿var assert = require('assert');
+var diamond = require('../Modules/Diamond.js');
+
+describe('Diamond', function() {
+    it('A returns A', function () {
+        var result = diamond.produce('A');
+        assert.ok(result === 'A', "The only row should be A");
+    })
+
+    it('B returns diamond', function() {
+        var result = diamond.produce('B');
+        var splitted = result.split('\n');
+        
+        assert.ok(splitted[0] === ' A ', "First row should be A");
+        assert.ok(splitted[1] === 'B B', "Second row should be B B");
+        assert.ok(splitted[2] === ' A ', "Last row should be A");
+    })
+})
